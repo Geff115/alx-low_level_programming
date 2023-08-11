283a3ed0897d38a159cfdd4c4ae122d007438f16
Geff115 <gabrielnoah129@gmail.com>
2023-08-11 03:35:44 +0100

    My first line in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
-/* writing a C program without using printf */
 #include <stdio.h>
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle. \n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
