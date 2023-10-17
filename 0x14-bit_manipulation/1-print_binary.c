c53f5cd32c3c0725b19bd4c685d9b908e4c87c0a
Geff115 <gabrielnoah129@gmail.com>
2023-10-17 07:19:45 -0700

    A function that prints the binary rep of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,52 +1,27 @@
 #include "main.h"
 
 /**
- * pow_func - func calculates (base ^ power)
- * @bs: base of the exponent
- * @pw: power of the exponent
- *
- * Return: value of (bs ^ pw)
- */
-unsigned long int pow_func(unsigned int bs, unsigned int pw)
-{
-	unsigned long int vm;
-	unsigned int k;
-
-	vm = 1;
-	for (k = 1; k <= pw; k++)
-		vm *= bs;
-	return (vm);
-}
-
-/**
- * print_binary - prints a number in binary notation
- * @n: number to print
- * @fg: A character variable
- * @div: positive integer
- * @ch: positive integer
- *
- * Return: void
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int div, ch;
-	char fg;
+	int i, count = 0;
+	unsigned long int current;
 
-	fg = 0;
-	div = pow_func(2, sizeof(unsigned long int) * 8 - 1);
-	while (div != 0)
+	for (i = 63; i >= 0; i--)
 	{
-		ch = n & div;
-		if (!(ch != div))
+		current = n >> i;
+
+		if (current & 1)
 		{
-			fg = 1;
 			_putchar('1');
+			count++;
 		}
-		else if (fg == 1 || div == 1)
-		{
+		else if (count)
 			_putchar('0');
-		}
-		div /= 1;
 	}
+	if (count == 0)
+		_putchar('0');
 }
 
