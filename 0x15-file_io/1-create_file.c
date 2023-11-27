ae7a374212bb129fab52d750fbfe3597b6565bc3
Geff115 <gabrielnoah129@gmail.com>
2023-11-27 18:21:02 +0100

    checked some errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,18 +13,18 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
-	if (text_content != NULL)
-		clo = strlen(text_content);
-
 	mar = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	g = write(mar, text_content, clo);
-
 	if (mar == -1)
 		return (-1);
-	if (g == -1)
+	if (text_content != NULL)
+	{
+		clo = strlen(text_content);
+		g = write(mar, text_content, clo);
+		if (g == -1)
+			return (-1);
+	}
+	if (close(mar) == -1)
 		return (-1);
-	close(mar);
 	return (1);
 }
 
