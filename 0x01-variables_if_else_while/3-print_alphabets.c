bcbc546a99ab442145abe5acee3f5db3307c2b48
gabrielnoah129@gmail.com <Geff115>
2024-03-15 10:35:45 +0100

    Alphabets in lowercase and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,9 +12,13 @@ int main(void)
 	int i, j;
 
 	for (i = 97; i <= 122; i++)
-	       putchar(i);
+	{
+		putchar(i);
+	}
 	for (j = 65; j <= 90; j++)
+	{
 		putchar(j);
+	}
 	putchar('\n');
 	return (0);
 }
