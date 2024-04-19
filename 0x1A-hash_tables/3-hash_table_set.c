81033a42e74902782a8ac83dc0cd6c7bdfbf9257
gabrielnoah129@gmail.com <Geff115>
2024-04-19 02:52:57 +0100

    Adds an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,65 @@
+#include "hash_tables.h"
+
+/**
+ * create_new_node - A function that creates a new node and maps the
+ * key to its value.
+ * @key: An string to be mapped to its hash value.
+ * @value: Value associated with the key.
+ *
+ * Return: The new node.
+ */
+hash_node_t *create_new_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Couldn't allocate memory for new node\n");
+		return (NULL);
+	}
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
+ * hash_table_set - A function that adds an element to a hash table
+ * and handles collision.
+ * @ht: A pointer to a hash table.
+ * @key: A string to be mapped to its hash value.
+ * @value: Value associated with the key.
+ *
+ * Return: (1) successful.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node, *temp;
+	unsigned long int index;
+
+	index = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+	new_node = create_new_node(key, value);
+	if (ht->array[index] != NULL)
+		new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
