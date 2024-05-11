9107e1354f2c6758b9a78266e49a6531823b231d
gabrielnoah129@gmail.com <Geff115>
2024-05-11 05:49:34 +0100

    Increased the length of the write function to include the newline

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
 int main(void)
 {
 write(2,
-"and that piece of art is useful\" - Dorar Korpar, 2015-10-19\n", 60);
+"and that piece of art is useful\" - Dorar Korpar, 2015-10-19", 59);
+write(2, "\n", 1);
 return (1);
 }
 
